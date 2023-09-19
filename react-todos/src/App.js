100d5d026ea2de130e2b95d73abdf490fdfb36bf
Ranjan Chaubey <chaubeyranjan8@gmail.com>
2023-09-19 23:07:08 +0530

    Adding-onClick-event-listeners

--- react-todos/src/App.js
+++ react-todos/src/App.js
@@ -11,6 +11,18 @@ function App() {
 
   ]
 
+  const addTodo = ()=>{
+    if(todos.length>0){
+      const newTodo = {
+        rowNumber: todos.length + 1,
+        rowDescription: 'New Todo',
+        rowAssigned: 'User Three'
+      };
+      todos.push(newTodo);
+      console.log(todos);
+    }
+  }
+
   return (
     <div className='mt-5 container'>
       <div className='card'>
@@ -19,6 +31,7 @@ function App() {
         </div>
         <div className='card-body'>
          <TodoTable todos={todos}/>
+         <button class='btn btn-primary' onClick={addTodo}>Add new Todo</button>
         </div>
       </div>
     </div>
