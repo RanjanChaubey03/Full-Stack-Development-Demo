4d9b030a020994a5b9bed9fbfbc492bbffbec5dc
Ranjan Chaubey <chaubeyranjan8@gmail.com>
2023-09-30 13:50:30 +0530

    typescript-class

---
+++ typescript-training/04-classes/01-class-definitions/Customer.js
@@ -0,0 +1,10 @@
+var Customer = /** @class */ (function () {
+    function Customer() {
+    }
+    return Customer;
+}());
+var myCustomer = new Customer();
+myCustomer.firstName = "Ranjan";
+myCustomer.lastname = "Chaubey";
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastname);
---
+++ typescript-training/04-classes/01-class-definitions/Customer.ts
@@ -0,0 +1,13 @@
+class Customer{
+    firstName : string;
+    lastname : string;
+
+}
+
+let myCustomer = new Customer();
+
+myCustomer.firstName = "Ranjan";
+myCustomer.lastname = "Chaubey";
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastname);
