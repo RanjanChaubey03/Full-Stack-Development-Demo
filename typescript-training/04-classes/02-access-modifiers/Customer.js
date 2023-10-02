d8e3250c87405658658d9491720085468cebd3f4
Ranjan Chaubey <chaubeyranjan8@gmail.com>
2023-10-02 21:36:54 +0530

    typescript-classes-access-modifiers

---
+++ typescript-training/04-classes/02-access-modifiers/Customer.js
@@ -0,0 +1,30 @@
+var Customer = /** @class */ (function () {
+    function Customer(theFirst, theSecond) {
+        this._firstName = theFirst;
+        this._lastname = theSecond;
+    }
+    Object.defineProperty(Customer.prototype, "firstName", {
+        get: function () {
+            return this._firstName;
+        },
+        set: function (value) {
+            this._firstName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Customer.prototype, "lastname", {
+        get: function () {
+            return this._lastname;
+        },
+        set: function (value) {
+            this._lastname = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Customer;
+}());
+var myCustomer = new Customer("Ranjan", "Chaturvedi");
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastname);
