fd008fb34d196080216f072fefb6abc62033fa97
Ranjan Chaubey <chaubeyranjan8@gmail.com>
2023-10-02 21:13:29 +0530

    typescript-classes-with-constructor

--- typescript-training/04-classes/01-class-definitions/Customer.js
+++ typescript-training/04-classes/01-class-definitions/Customer.js
@@ -1,10 +1,10 @@
 var Customer = /** @class */ (function () {
-    function Customer() {
+    function Customer(theFirst, theSecond) {
+        this.firstName = theFirst;
+        this.lastname = theSecond;
     }
     return Customer;
 }());
-var myCustomer = new Customer();
-myCustomer.firstName = "Ranjan";
-myCustomer.lastname = "Chaubey";
+var myCustomer = new Customer("Ranjan", "Chaubey");
 console.log(myCustomer.firstName);
 console.log(myCustomer.lastname);
--- typescript-training/04-classes/01-class-definitions/Customer.ts
+++ typescript-training/04-classes/01-class-definitions/Customer.ts
@@ -2,12 +2,14 @@ class Customer{
     firstName : string;
     lastname : string;
 
+    constructor(theFirst:string,theSecond:string){
+        this.firstName=theFirst;
+        this.lastname=theSecond;
+    }
+
 }
 
-let myCustomer = new Customer();
-
-myCustomer.firstName = "Ranjan";
-myCustomer.lastname = "Chaubey";
+let myCustomer = new Customer("Ranjan","Chaubey");
 
 console.log(myCustomer.firstName);
 console.log(myCustomer.lastname);
