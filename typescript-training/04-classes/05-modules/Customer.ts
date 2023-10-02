b1c6b33049f90dbabc8a63da4e259298d45b82df
Ranjan Chaubey <chaubeyranjan8@gmail.com>
2023-10-02 22:14:37 +0530

    typescript-classes-modules

---
+++ typescript-training/04-classes/05-modules/Customer.ts
@@ -0,0 +1,23 @@
+export class Customer{
+    
+
+    constructor(private _firstName:string,private _lastname:string){
+       
+    }
+
+    public get firstName():string{
+        return this._firstName;
+    }
+
+    public set firstName(value:string){
+        this._firstName=value;
+    }
+
+    public get lastname(): string {
+        return this._lastname;
+    }
+    public set lastname(value: string) {
+        this._lastname = value;
+    }
+
+}
---
+++ typescript-training/04-classes/05-modules/Driver.ts
@@ -0,0 +1,5 @@
+import {Customer} from "./Customer";
+let myCustomer = new Customer("Ranjan","Chaturvedi");
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastname);
